e0c18d93ff3da68e481d461a49ad02f9917a1dc7
mfauvell <mfauvell@dismental.org>
2018-09-14 22:59:38 +0200

    Version 1.1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # yauesc
-Creator of Uned exams schedules for my PFG.
+
+This project proposes the resolution of exam schedules for the National University of Distance Education, using a genetic algorithm to calculate it.
+
+Planning of schedules is a common problem of those considered intractable (NP), in addition to the specific problem of design of examin schedules of the UNED, it is more complicated by its unique characteristics. In a week (from Monday to Friday), in four sessions per day, the exam schedules of subjects of 27 undergraduate and 52 master's degrees are concentrated. The resolution of these calendars entails a great computational task where it is not at all easy to reach an optimal solution. The use of genetic algorithms tries to alleviate this aspect.
+
+Genetic algorithms are a type of computing that is based on the idea of ​​the evolution of species to find optimal solutions in a broad search space. Each individual of the population to evolve is a solution, so through successive generations of crosses and occasional mutations the most suitable descendants survive and guide the evolution towards more adapted solutions, therefore more optimal.
+
+In this project an application has been developed capable of solving the UNED exam calendars using several types of genetic algorithms. To do this, a modern methodology has been followed and quality software capable of working on multiple platforms has been obtained.
--- src/main/java/es/uned/yauesc/geneticAlgorithm/GeneticAlgorithmDefaultConfig.java
+++ src/main/java/es/uned/yauesc/geneticAlgorithm/GeneticAlgorithmDefaultConfig.java
@@ -6,35 +6,35 @@ package es.uned.yauesc.geneticAlgorithm;
 public class GeneticAlgorithmDefaultConfig {
 
 	public static final boolean isParallel = true;
-	public static final int generations = 50;
-	public static final int generationsToMigrate = 10;
+	public static final int generations = 500;
+	public static final int generationsToMigrate = 50;
 	public static final int numberMigrants = 10;
 	
 	//First Algorithm
 	public static final ParentSelectorType parentSelectorFirst = ParentSelectorType.Ranking;
 	public static final double sParameterFirst = 1.5;
 	public static final RecombinationOperatorType recombinationOperatorFirst = RecombinationOperatorType.Uniform;
-	public static final double probabilityRecombinationFirst = 0.8;
+	public static final double probabilityRecombinationFirst = 0.9;
 	public static final MutationOperatorType mutationOperatorFirst = MutationOperatorType.Simple;
-	public static final double probabilityMutationFirst = 0.001;
-	public static final SurvivorSelectorType survivorSelectorFirst = SurvivorSelectorType.SteadyState;
+	public static final double probabilityMutationFirst = 0.2;
+	public static final SurvivorSelectorType survivorSelectorFirst = SurvivorSelectorType.RoundRobin;
 	public static int survivorsFirst = 4;
 	public static int numberBattleFirst = 5;
-	public static int populationSizeFirst = 150;
+	public static int populationSizeFirst = 100;
 	public static int maxPopulationFirst = 200;
 	public static int minPopulationFirst = 100;
 	
 	//Second Algorithm
 	public static final ParentSelectorType parentSelectorSecond = ParentSelectorType.Tournament;
 	public static final double sParameterSecond = 1.5;
-	public static final RecombinationOperatorType recombinationOperatorSecond = RecombinationOperatorType.Uniform;
+	public static final RecombinationOperatorType recombinationOperatorSecond = RecombinationOperatorType.Simple;
 	public static final double probabilityRecombinationSecond = 0.8;
 	public static final MutationOperatorType mutationOperatorSecond = MutationOperatorType.Permutation;
-	public static final double probabilityMutationSecond = 0.1;
+	public static final double probabilityMutationSecond = 0.01;
 	public static final SurvivorSelectorType survivorSelectorSecond = SurvivorSelectorType.SteadyState;
-	public static int survivorsSecond = 4;
+	public static int survivorsSecond = 2;
 	public static int numberBattleSecond = 5;
-	public static int populationSizeSecond = 150;
+	public static int populationSizeSecond = 100;
 	public static int maxPopulationSecond = 200;
 	public static int minPopulationSecond = 100;
 	
@@ -42,13 +42,13 @@ public class GeneticAlgorithmDefaultConfig {
 	public static final ParentSelectorType parentSelectorThird = ParentSelectorType.Tournament;
 	public static final double sParameterThird = 1.5;
 	public static final RecombinationOperatorType recombinationOperatorThird = RecombinationOperatorType.Uniform;
-	public static final double probabilityRecombinationThird = 0.9;
+	public static final double probabilityRecombinationThird = 0.8;
 	public static final MutationOperatorType mutationOperatorThird = MutationOperatorType.Permutation;
 	public static final double probabilityMutationThird = 0.01;
-	public static final SurvivorSelectorType survivorSelectorThird = SurvivorSelectorType.RoundRobin;
+	public static final SurvivorSelectorType survivorSelectorThird = SurvivorSelectorType.AgeBased;
 	public static int survivorsThird = 4;
 	public static int numberBattleThird = 5;
-	public static int populationSizeThird = 150;
-	public static int maxPopulationThird = 200;
-	public static int minPopulationThird = 100;
+	public static int populationSizeThird = 100;
+	public static int maxPopulationThird = 125;
+	public static int minPopulationThird = 75;
 }
